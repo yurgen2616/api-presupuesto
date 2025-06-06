363bc8454e4047cd9c46fe2eb6808d6bd9e6261e
yurgen2616 <ypradol@ufpso.edu.co>
2025-06-06 17:09:46 -0500

    ➕Se implementa lógica en PresupuestoService

--- src/main/java/com/api/presupuesto/api_presupuesto/service/PresupuestoService.java
+++ src/main/java/com/api/presupuesto/api_presupuesto/service/PresupuestoService.java
@@ -1,5 +1,14 @@
 package com.api.presupuesto.api_presupuesto.service;
 
+import java.util.List;
+import com.api.presupuesto.api_presupuesto.model.Presupuesto;
+
 public interface PresupuestoService {
+
+    Presupuesto save(Presupuesto presupuesto);
+    List<Presupuesto> findAll();
+    Presupuesto findById(Long id);
+    Presupuesto update(Long id, Presupuesto presupuesto);
+    void delete(Long id);
 
 }
