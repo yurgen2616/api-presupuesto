4dc5bea87c86268c65b4f5356db2c24f37edfc4d
yurgen2616 <ypradol@ufpso.edu.co>
2025-06-06 15:54:05 -0500

    ➕Se agrega el proyecto

---
+++ src/main/java/com/api/presupuesto/api_presupuesto/ApiPresupuestoApplication.java
@@ -0,0 +1,13 @@
+package com.api.presupuesto.api_presupuesto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPresupuestoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPresupuestoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/api/presupuesto/api_presupuesto/ApiPresupuestoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.presupuesto.api_presupuesto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiPresupuestoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
